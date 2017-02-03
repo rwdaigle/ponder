153b79b994702cb1e6b9428e4146a7fe0ca98bb9
Ryan Daigle <ryan.daigle@gmail.com>
2017-02-03 11:11:32 -0500

    Integrate bootstrap theme

--- lib/reviewcast/endpoint.ex
+++ lib/reviewcast/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Reviewcast.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :reviewcast, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts img js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
